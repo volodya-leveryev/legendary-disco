1a66ab5272cd681414baf7e95abeea72c77ae490
Volodya Leveryev <volodya.leveryev@gmail.com>
2021-03-22 10:41:08 +0900

    Добавлена авторизация через Google

--- jimmy/__init__.py
+++ jimmy/__init__.py
@@ -1,23 +1,27 @@
-import os
-
 from flask import Flask
 from flask_admin import Admin
+from flask_dance.contrib.google import make_google_blueprint
 
-from jimmy.models import db, Person, JobAssignment, StudentGroup
-from jimmy.views import (PersonView, TeacherView, StudentGroupView)
+from jimmy import models, views
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_envvar('JIMMY_CONFIG')
 
-    app.config['SECRET_KEY'] = os.environ.get('APP_SECRET', 'secret')
-    app.config['MONGODB_HOST'] = os.environ.get('APP_DB_HOST', 'localhost')
-    app.config['MONGODB_DB'] = 'jimmy'
-    db.init_app(app)
+    models.db.init_app(app)
+
+    scope = ['openid', 'https://www.googleapis.com/auth/userinfo.email']
+    auth_bp = make_google_blueprint(scope=scope)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    app.add_url_rule('/', endpoint='home', view_func=views.home_page)
+    app.add_url_rule('/login/', endpoint='login', view_func=views.login_page)
+    app.add_url_rule('/logout/', view_func=views.logout)
 
     admin = Admin(app)
-    admin.add_view(PersonView(Person, 'Люди'))
-    admin.add_view(TeacherView(JobAssignment, 'Преподаватели'))
-    admin.add_view(StudentGroupView(StudentGroup, 'Учебные группы'))
+    admin.add_view(views.PersonView(models.Person, 'Люди'))
+    admin.add_view(views.TeacherView(models.JobAssignment, 'Преподаватели'))
+    admin.add_view(views.StudentGroupView(models.StudentGroup, 'Учебные группы'))
 
     return app
--- jimmy/views.py
+++ jimmy/views.py
@@ -1,4 +1,44 @@
+import json
+
+from flask import redirect, render_template, request, session, url_for
 from flask_admin.contrib.mongoengine import ModelView
+from flask_dance.contrib.google import google
+
+from jimmy.models import Person
+
+
+def login_required(func):
+    def wrap(*args, **kwargs):
+        if not google.authorized:
+            return redirect(url_for('login'))
+        if 'user' not in session:
+            userinfo = json.loads(google.get('/oauth2/v1/userinfo').content)
+            persons = Person.objects(emails=userinfo.get('email'))
+            if not persons:
+                return redirect(url_for('login'))
+            session['user'] = {
+                'id': str(persons[0].id),
+                'last_name': persons[0].last_name,
+                'first_name': persons[0].first_name,
+                'second_name': persons[0].second_name,
+            }
+        return func(*args, **kwargs)
+    return wrap
+
+
+@login_required
+def home_page():
+    return render_template('index.html')
+
+
+def login_page():
+    return render_template('login.html')
+
+
+def logout():
+    session.pop('google_oauth_token')
+    session.pop('user')
+    return redirect(url_for('login'))
 
 
 class PersonView(ModelView):
@@ -11,6 +51,12 @@ class PersonView(ModelView):
         'title': 'Учёное звание',
     }
 
+    def is_accessible(self):
+        return 'user' in session
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login', next=request.url))
+
 
 class TeacherView(ModelView):
     column_default_sort = 'person'
@@ -22,9 +68,21 @@ class TeacherView(ModelView):
         'wage_rate': 'Ставка',
     }
 
+    def is_accessible(self):
+        return 'user' in session
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login', next=request.url))
+
 
 class StudentGroupView(ModelView):
     column_default_sort = 'name'
     column_labels = {
         'name': 'Название',
     }
+
+    def is_accessible(self):
+        return 'user' in session
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login', next=request.url))
